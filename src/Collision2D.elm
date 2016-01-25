49728ff5d153e2bb1aff2e32124df2ed9ba61d83
Nick Hollon <falling.masonry@gmail.com>
2016-01-25 14:55:12 -0800

    update to 0.16, remove test dependencies

--- src/Collision2D.elm
+++ src/Collision2D.elm
@@ -12,7 +12,6 @@ module Collision2D (isOutside, isInside, fromVectors, Hull) where
 @docs Hull
 -}
 
-
 import Math.Vector2 as Vec2 exposing (Vec2)
 
 
@@ -23,38 +22,43 @@ Returns an empty hull if given less than three vertexes.
 -}
 fromVectors : List Vec2 -> Hull
 fromVectors vertexes =
-  let
-    segments =
-      (List.drop 1 vertexes) ++ (List.take 1 vertexes)
-        |> List.map2 (,) vertexes
-  in
-    if | List.length vertexes >= 3 -> fromSegments segments
-       | otherwise -> Bounded []
+    let
+        segments =
+            (List.drop 1 vertexes)
+                ++ (List.take 1 vertexes)
+                |> List.map2 (,) vertexes
+    in
+        if List.length vertexes >= 3 then
+            fromSegments segments
+        else
+            Bounded []
 
 
-fromSegments : List (Vec2, Vec2) -> Hull
+fromSegments : List ( Vec2, Vec2 ) -> Hull
 fromSegments segments =
-  let
-    toNormal a b =
-      let (x, y) = Vec2.toTuple (Vec2.direction a b)
-      in Vec2.fromTuple (-y, x)
+    let
+        toNormal a b =
+            let
+                ( x, y ) = Vec2.toTuple (Vec2.direction a b)
+            in
+                Vec2.fromTuple ( -y, x )
 
-    toSide (a, b) =
-      { keyPoint = a
-      , normal = toNormal a b
-      }
+        toSide ( a, b ) =
+            { keyPoint = a
+            , normal = toNormal a b
+            }
 
-    isDefined vec =
-      not (isNaN (Vec2.getX vec) || isNaN (Vec2.getY vec))
-  in
-    List.map toSide segments
-      |> List.filter (.normal >> isDefined)
-      |> Bounded
+        isDefined vec =
+            not (isNaN (Vec2.getX vec) || isNaN (Vec2.getY vec))
+    in
+        List.map toSide segments
+            |> List.filter (.normal >> isDefined)
+            |> Bounded
 
-         
+
 {-| Returns `True` if the given position is on or in the given hull.
 Defaults to `False` if the hull has no sides.
-    
+
     import Math.Vector2 exposing (vec2)
 
     hull =
@@ -73,25 +77,27 @@ Defaults to `False` if the hull has no sides.
 -}
 isInside : Vec2 -> Hull -> Bool
 isInside point (Bounded sides) =
-  let
-    isBehind side =
-      Vec2.dot side.normal (Vec2.sub point side.keyPoint) < 1e-6
-  in
-    not (List.isEmpty sides) && List.all isBehind sides
+    let
+        isBehind side =
+            Vec2.dot side.normal (Vec2.sub point side.keyPoint) < 1.0e-6
+    in
+        not (List.isEmpty sides) && List.all isBehind sides
 
 
 {-| Returns `True` if the given position is outside the given hull.
 The logical inverse of `isInside`.
 -}
 isOutside : Vec2 -> Hull -> Bool
-isOutside point boundary = not (isInside point boundary)
+isOutside point boundary =
+    not (isInside point boundary)
 
 
 {-| A collection of sides that together represent a hull. This library
 interprets the sides as forming the smallest possible convex polygon.
 -}
-type Hull =
-  Bounded (List Side)
+type Hull
+    = Bounded (List Side)
+
 
 
 {- A side is a straight-line boundary with an inside and an outside. The key
@@ -105,6 +111,9 @@ be equivalent sometimes.
 Since the sides have no endpoints, infinite-area hulls are possible (e.g. if
 there are less than three sides).
 -}
+
+
 type alias Side =
-  { keyPoint : Vec2
-  , normal : Vec2 }
+    { keyPoint : Vec2
+    , normal : Vec2
+    }
